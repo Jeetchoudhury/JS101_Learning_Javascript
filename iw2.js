2cc9b2590a6aa94a7feefd6f995a767faf3716eb
Santosh Lal Choudhury <jeetchoudhury76@gmail.com>
2023-04-11 10:40:47 +0530

    ternary & logical iw coding

---
+++ iw2.js
@@ -0,0 +1,14 @@
+Problem 1: Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+sol:https://replit.com/@jeetchoudhury/web27-variable#condi5.js
+
+Problem 2 : Given any character, if it is a vowel print "Vowel"
+sol:https://replit.com/@jeetchoudhury/web27-variable#condi6.js
+
+Problem 3 : Given and character if it is a consonant print "Consonant"
+sol:https://replit.com/@jeetchoudhury/web27-variable#condi7.js
+
+Problem 4: Given 3 numbers (all different values), print which is greatest
+sol:https://replit.com/@jeetchoudhury/web27-variable#condi8.js
+
+Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+sol:https://replit.com/@jeetchoudhury/web27-variable#condi9.js
